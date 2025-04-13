3471b5b32741868ecc9cbf73f0f7fa27da7023e6
SBTopZZZ-LG <sbtopinkatti@gmail.com>
2025-04-13 23:32:33 +0530

    ci: fix semantic-release commit validation issues

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'body-max-line-length': [2, 'always', 100],
+    'body-max-line-length': [1, 'always', 200], // Changed from error(2) to warning(1) and increased length
   },
+  ignores: [
+    // Ignore semantic-release commit messages
+    (commit) => commit.includes('[skip ci]')
+  ],
 };
